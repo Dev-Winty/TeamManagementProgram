0f98152a975cf2b4fc2eccea0dfce82c91b594f9
LimKyeongJoon <kyeongjoon16881688@gmail.com>
2020-08-24 12:06:25 +0900

    Mysql연결
    Mysql에 연결을 하고 user테이블 안에 존재하는 레코드를 출력하는 코드 작성

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +25,7 @@ namespace TeamManagementProgram
     {
         public const string TITLE_NAME = "팀원 관리 프로그램";
         List<User> users = new List<User>();
-       // List<User> cbUsers = new List<User>();
+        // List<User> cbUsers = new List<User>();
         public MainWindow()
         {
             InitializeComponent();
@@ -32,7 +34,7 @@ namespace TeamManagementProgram
             //cbUsers.Add(new User() { Team = "윈도우", Name = "임경준" });
             lsMember.ItemsSource = users;
             cbRmName.ItemsSource = users;
-            
+
         }
 
         private void btnRmInList_Click(object sender, RoutedEventArgs e)
@@ -68,7 +70,7 @@ namespace TeamManagementProgram
                     }
                 }
                 users.Add(new User() { Team = tbAddTeam.Text, Name = tbAddName.Text });
-                lsMember.ItemsSource = users;             
+                lsMember.ItemsSource = users;
                 lsMember.Items.Refresh();
                 cbRmName.ItemsSource = users;
                 cbRmName.Items.Refresh();
@@ -97,6 +99,57 @@ namespace TeamManagementProgram
             }
         }
 
+        public void dbConnect(string insertQuery)
+        {
+            using (MySqlConnection connection = new MySqlConnection("Server=localhost;Port=3306;Database=teammanagementprogram;Uid=root;Pwd=winty0320;"))
+            {
+                try
+                {
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand(insertQuery, connection);
+                    if (command.ExecuteNonQuery() == 1)
+                    {
+                        tbLog.Text = tbLog.Text + "Query 전송 성공";
+                    } else
+                    {
+                        tbLog.Text = tbLog.Text + "Query 전송 실패";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("데이터베이스 오류" + Environment.NewLine + ex.Message, TITLE_NAME);
+                }
+            }
+        }
+
+        public void selectDb()
+        {
+            using (MySqlConnection connection = new MySqlConnection("Server=localhost;Port=3306;Database=teammanagementprogram;Uid=root;Pwd=winty0320;"))
+            {
+                try
+                {
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand("SELECT * FROM user;", connection);
+                    MySqlDataReader reader = command.ExecuteReader();
+
+                    while(reader.Read())
+                    {
+                        MessageBox.Show(reader["team"] + " " + reader["name"]);
+                    }
+                    reader.Close();
+                } catch (Exception ex)
+                {
+                    MessageBox.Show("데이터베이스 오류" + Environment.NewLine + ex.Message, TITLE_NAME);
+                }
+            }
+        }
+
+        private void btnConnectTest_Click(object sender, RoutedEventArgs e)
+        {
+            //dbConnect("SHOW TABLES;");
+            selectDb();
+        }
+
         public class User
         {
             public string Team { get; set; }
@@ -105,7 +158,7 @@ namespace TeamManagementProgram
 
             public override string ToString()
             {
-                return this.Team + " " + this.Name; 
+                return this.Team + " " + this.Name;
             }
         }
 
